aa609ffc842162a588340bb528c2243e0f90cdba
Jdevvv <llanta.jeremy@gmail.com>
2020-06-16 11:16:57 +0200

    IPod UI

--- lib/main.dart
+++ lib/main.dart
@@ -1,117 +1,217 @@
 import 'package:flutter/material.dart';
 
-void main() {
-  runApp(MyApp());
-}
+List images = [
+  'https://d1csarkz8obe9u.cloudfront.net/posterpreviews/artistic-album-cover-design-template-d12ef0296af80b58363dc0deef077ecc_screen.jpg?ts=1561488440',
+  'https://i.pinimg.com/originals/b4/75/00/b4750046d94fed05d00dd849aa5f0ab7.jpg',
+  'https://i.pinimg.com/originals/3a/f0/e5/3af0e55ea66ea69e35145fb108b4a636.jpg',
+  'https://images-na.ssl-images-amazon.com/images/I/61O0xzf8l5L._AC_SX466_.jpg',
+  'https://cms-assets.tutsplus.com/uploads/users/114/posts/34296/image/Final-image.jpg',
+  'https://d1csarkz8obe9u.cloudfront.net/posterpreviews/cool-music-album-cover-design-template-3324b2b5c69bb9a3cfaed14c71f24ca8_screen.jpg?ts=1572456482',
+  'https://i.pinimg.com/originals/c5/30/52/c53052a1bfbd92bcd48b63389d5de8bf.jpg',
+  'https://d8mkdcmng3.imgix.net/39c1/home-and-garden-home-decor-signs-and-wall-art-nirvana-smiley-12-inch-album-cover-framed-print.jpg?auto=format&bg=0FFF&fit=fill&h=600&q=100&w=600&s=3c4c9eb861366f3c70cd4347d0e02b93',
+  'https://www.thebackpackerz.com/wp-content/uploads/2019/10/vald-ce-monde-est-cruel-album-cover.jpg'
+];
+
+void main() => runApp(MyApp());
 
 class MyApp extends StatelessWidget {
-  // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo',
-      theme: ThemeData(
-        // This is the theme of your application.
-        //
-        // Try running your application with "flutter run". You'll see the
-        // application has a blue toolbar. Then, without quitting the app, try
-        // changing the primarySwatch below to Colors.green and then invoke
-        // "hot reload" (press "r" in the console where you ran "flutter run",
-        // or simply save your changes to "hot reload" in a Flutter IDE).
-        // Notice that the counter didn't reset back to zero; the application
-        // is not restarted.
-        primarySwatch: Colors.blue,
-        // This makes the visual density adapt to the platform that you run
-        // the app on. For desktop platforms, the controls will be smaller and
-        // closer together (more dense) than on mobile platforms.
-        visualDensity: VisualDensity.adaptivePlatformDensity,
+      theme: ThemeData(brightness: Brightness.dark),
+      home: Scaffold(
+        body: IPod(),
       ),
-      home: MyHomePage(title: 'Flutter Demo Home Page'),
     );
   }
 }
 
-class MyHomePage extends StatefulWidget {
-  MyHomePage({Key key, this.title}) : super(key: key);
-
-  // This widget is the home page of your application. It is stateful, meaning
-  // that it has a State object (defined below) that contains fields that affect
-  // how it looks.
-
-  // This class is the configuration for the state. It holds the values (in this
-  // case the title) provided by the parent (in this case the App widget) and
-  // used by the build method of the State. Fields in a Widget subclass are
-  // always marked "final".
-
-  final String title;
+class IPod extends StatefulWidget {
+  IPod({Key key}) : super(key: key);
 
   @override
-  _MyHomePageState createState() => _MyHomePageState();
+  _IPodState createState() => _IPodState();
 }
 
-class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
+class _IPodState extends State<IPod> {
+  final PageController _pageCtrl = PageController(viewportFraction: 0.6);
 
-  void _incrementCounter() {
-    setState(() {
-      // This call to setState tells the Flutter framework that something has
-      // changed in this State, which causes it to rerun the build method below
-      // so that the display can reflect the updated values. If we changed
-      // _counter without calling setState(), then the build method would not be
-      // called again, and so nothing would appear to happen.
-      _counter++;
+  double currentPage = 0.0;
+
+  @override
+  void initState() {
+    _pageCtrl.addListener(() {
+      setState(() {
+        currentPage = _pageCtrl.page;
+      });
     });
+    super.initState();
   }
 
   @override
   Widget build(BuildContext context) {
-    // This method is rerun every time setState is called, for instance as done
-    // by the _incrementCounter method above.
-    //
-    // The Flutter framework has been optimized to make rerunning build methods
-    // fast, so that you can just rebuild anything that needs updating rather
-    // than having to individually change instances of widgets.
-    return Scaffold(
-      appBar: AppBar(
-        // Here we take the value from the MyHomePage object that was created by
-        // the App.build method, and use it to set our appbar title.
-        title: Text(widget.title),
+    return SafeArea(
+      child: Column(
+        children: <Widget>[
+          Container(
+            height: 300,
+            color: Colors.black,
+            child: PageView.builder(
+              controller: _pageCtrl,
+              scrollDirection: Axis.horizontal,
+              itemCount: 9, //Colors.accents.length,
+              itemBuilder: (context, int currentIdx) {
+                return AlbumCard(
+                  color: Colors.accents[currentIdx],
+                  idx: currentIdx,
+                  currentPage: currentPage,
+                );
+              },
+            ),
+          ),
+          Spacer(),
+          Center(
+            child: Stack(
+              alignment: Alignment.center,
+              children: [
+                GestureDetector(
+                  onPanUpdate: _panHandler,
+                  child: Container(
+                    height: 300,
+                    width: 300,
+                    decoration: BoxDecoration(
+                      shape: BoxShape.circle,
+                      color: Colors.black,
+                    ),
+                    child: Stack(children: [
+                      Container(
+                        child: Text(
+                          'MENU',
+                          style: TextStyle(
+                              fontSize: 24, fontWeight: FontWeight.bold),
+                        ),
+                        alignment: Alignment.topCenter,
+                        margin: EdgeInsets.only(top: 36),
+                      ),
+                      Container(
+                        child: IconButton(
+                          icon: Icon(Icons.fast_forward),
+                          iconSize: 40,
+                          onPressed: () => _pageCtrl.animateToPage(
+                              (_pageCtrl.page + 1).toInt(),
+                              duration: Duration(milliseconds: 300),
+                              curve: Curves.easeIn),
+                        ),
+                        alignment: Alignment.centerRight,
+                        margin: EdgeInsets.only(right: 30),
+                      ),
+                      Container(
+                        child: IconButton(
+                          icon: Icon(Icons.fast_rewind),
+                          iconSize: 40,
+                          onPressed: () => _pageCtrl.animateToPage(
+                              (_pageCtrl.page - 1).toInt(),
+                              duration: Duration(milliseconds: 300),
+                              curve: Curves.easeIn),
+                        ),
+                        alignment: Alignment.centerLeft,
+                        margin: EdgeInsets.only(left: 30),
+                      ),
+                      Container(
+                        child: Icon(
+                          Icons.play_arrow,
+                          size: 40,
+                        ),
+                        alignment: Alignment.bottomCenter,
+                        margin: EdgeInsets.only(bottom: 30),
+                      )
+                    ]),
+                  ),
+                ),
+                Container(
+                  height: 100,
+                  width: 100,
+                  decoration: BoxDecoration(
+                    shape: BoxShape.circle,
+                    color: Colors.white38,
+                  ),
+                ),
+              ],
+            ),
+          ),
+          Spacer(),
+        ],
       ),
-      body: Center(
-        // Center is a layout widget. It takes a single child and positions it
-        // in the middle of the parent.
-        child: Column(
-          // Column is also a layout widget. It takes a list of children and
-          // arranges them vertically. By default, it sizes itself to fit its
-          // children horizontally, and tries to be as tall as its parent.
-          //
-          // Invoke "debug painting" (press "p" in the console, choose the
-          // "Toggle Debug Paint" action from the Flutter Inspector in Android
-          // Studio, or the "Toggle Debug Paint" command in Visual Studio Code)
-          // to see the wireframe for each widget.
-          //
-          // Column has various properties to control how it sizes itself and
-          // how it positions its children. Here we use mainAxisAlignment to
-          // center the children vertically; the main axis here is the vertical
-          // axis because Columns are vertical (the cross axis would be
-          // horizontal).
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            Text(
-              'You have pushed the button this many times:',
+    );
+  }
+
+  void _panHandler(DragUpdateDetails d) {
+    /// Pan movements
+    bool panUp = d.delta.dy <= 0.0;
+    bool panLeft = d.delta.dx <= 0.0;
+    bool panRight = !panLeft;
+    bool panDown = !panUp;
+
+    /// Pan location on the wheel
+    bool onTop = d.localPosition.dy <= 150; // 150 == radius of circle
+    bool onLeftSide = d.localPosition.dx <= 150;
+    bool onRightSide = !onLeftSide;
+    bool onBottom = !onTop;
+
+    /// Absoulte change on axis
+    double yChange = d.delta.dy.abs();
+    double xChange = d.delta.dx.abs();
+
+    /// Directional change on wheel
+    double vert = (onRightSide && panUp) || (onLeftSide && panDown)
+        ? yChange
+        : yChange * -1;
+
+    double horz =
+        (onTop && panLeft) || (onBottom && panRight) ? xChange : xChange * -1;
+
+    // Total computed change with velocity
+    double scrollOffsetChange = (horz + vert) * (d.delta.distance * 0.2);
+
+    // Move the page view scroller
+    _pageCtrl.jumpTo(_pageCtrl.offset + scrollOffsetChange);
+  }
+}
+
+class AlbumCard extends StatelessWidget {
+  final Color color;
+  final int idx;
+  final double currentPage;
+  AlbumCard({this.color, this.idx, this.currentPage});
+
+  @override
+  Widget build(BuildContext context) {
+    double relativePosition = idx - currentPage;
+
+    return Container(
+      width: 250,
+      child: Transform(
+        transform: Matrix4.identity()
+          ..setEntry(3, 2, 0.003) // add perspective
+          ..scale((1 - relativePosition.abs()).clamp(0.2, 0.6) + 0.4)
+          ..rotateY(relativePosition),
+        // ..rotateZ(relativePosition),
+        alignment: relativePosition >= 0
+            ? Alignment.centerLeft
+            : Alignment.centerRight,
+        child: Container(
+          margin: EdgeInsets.only(top: 20, bottom: 20, left: 5, right: 5),
+          padding: EdgeInsets.all(10),
+          decoration: BoxDecoration(
+            borderRadius: BorderRadius.circular(12),
+            color: color,
+            image: DecorationImage(
+              fit: BoxFit.cover,
+              image: NetworkImage(images[idx]),
             ),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.headline4,
-            ),
-          ],
+          ),
         ),
       ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: Icon(Icons.add),
-      ), // This trailing comma makes auto-formatting nicer for build methods.
     );
   }
 }
